874b719f55861a663aad9bef9cf03ec4ea81d26d
Thomas Weller <ThomasWeller@wellisolutions.de>
2021-12-30 23:17:31 +0100

    Add more SMART descriptions

--- Smart.Net/Resource.cs
+++ Smart.Net/Resource.cs
@@ -18,14 +18,22 @@
 0x0B,Calibration retry count
 0x0C,Power cycle count
 0x0D,Soft read error rate
+0xA8,SATA PHY Error Count
 0xAA,Available reserved space
 0xAB,Program fail count
 0xAC,Erase fail block count
 0xAD,Wear level count
 0xAE,Unexpected power loss count
+0xAF,Bad Cluster Table Count
+0xB1,Wear Leveling Count
+0xB3,Used Reserved Block Count (Total)
+0xB5,Program Fail Count (Total)
+0xB6,Erase Fail Count (Total)
 0xB7,SATA downshift count
 0xB8,End-to-End error
 0xBB,Uncorrectable error count
+0xBC,Command Timeout
+0xBD,High Fly Writes
 0xBE,Airflow Temperature
 0xBF,G-sense error rate
 0xC0,Unsafe shutdown count
@@ -46,6 +54,7 @@
 0xCF,Spin high current
 0xD0,Spin buzz
 0xD1,Offline seek performance
+0xDA,CRC Error Count
 0xDC,Disk shift
 0xDD,G-sense error rate
 0xDE,Loaded hours
@@ -56,9 +65,10 @@
 0xE3,Timer workload read/write ratio
 0xE4,Timer workload timer
 0xE6,GMR head amplitude
-0xE7,Temperature
+0xE7,Temperature / SSD Life Left
 0xE8,Available reserved space
 0xE9,Media wearout indicator
+0xEB,POR Recovery Count
 0xF0,Head flying hours
 0xF1,Life time writes
 0xF2,Life time reads
