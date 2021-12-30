400a2faca86417fe4663acb36f89d9d8001ad8c4
Thomas Weller <ThomasWeller@wellisolutions.de>
2021-12-30 22:55:36 +0100

    R - extract methods as propsed in TODO

--- Smart.Net/WmiController.cs
+++ Smart.Net/WmiController.cs
@@ -12,83 +12,15 @@ namespace Simplified.IO
             var drives = new DriveCollection();
             try
             {
-                // TODO: 2017-12-19 - Refactor regions into separate methods.
-                foreach (var device in new ManagementObjectSearcher(@"SELECT * FROM Win32_DiskDrive").Get())
+                foreach (var device in GetDevices())
                 {
                     var searcher = CreateSearcher();
                     var drive = GetDrive(device);
                     drive.DriveLetters = GetDriveLetters(device);
                     drive.IsOK = GetOverallStatus(searcher, drive);
-
-                    #region Smart Registers
-
                     drive.SmartAttributes.AddRange(Helper.GetSmartRegisters(Resource.SmartAttributes));
-
-                    searcher.Query = new ObjectQuery(@"Select * from MSStorageDriver_FailurePredictData Where InstanceName like ""%"
-                                                      + drive.PnpDeviceID.Replace("\\", "\\\\") + @"%""");
-
-                    foreach (ManagementObject data in searcher.Get())
-                    {
-                        var bytes = (byte[])data.Properties["VendorSpecific"].Value;
-                        for (var i = 0; i < 42; ++i)
-                        {
-                            try
-                            {
-                                int id = bytes[i * 12 + 2];
-
-                                int flags = bytes[i * 12 + 4]; // least significant status byte, +3 most significant byte, but not used so ignored.
-                                                               //bool advisory = (flags & 0x1) == 0x0;
-                                var failureImminent = (flags & 0x1) == 0x1;
-                                //bool onlineDataCollection = (flags & 0x2) == 0x2;
-
-                                int value = bytes[i * 12 + 5];
-                                int worst = bytes[i * 12 + 6];
-                                var vendordata = BitConverter.ToInt32(bytes, i * 12 + 7);
-                                if (id == 0) continue;
-
-                                var attr = drive.SmartAttributes.GetAttribute(id);
-                                attr.Current = value;
-                                attr.Worst = worst;
-                                attr.Data = vendordata;
-                                attr.IsOK = failureImminent == false;
-                            }
-                            catch (Exception ex)
-                            {
-                                // given key does not exist in attribute collection (attribute not in the dictionary of attributes)
-                                Debug.WriteLine(ex.Message);
-                            }
-                        }
-                    }
-
-                    searcher.Query = new ObjectQuery(@"Select * from MSStorageDriver_FailurePredictThresholds Where InstanceName like ""%"
-                                                      + drive.PnpDeviceID.Replace("\\", "\\\\") + @"%""");
-                    foreach (ManagementObject data in searcher.Get())
-                    {
-                        var bytes = (byte[])data.Properties["VendorSpecific"].Value;
-                        for (var i = 0; i < 42; ++i)
-                        {
-                            try
-                            {
-                                int id = bytes[i * 12 + 2];
-                                int thresh = bytes[i * 12 + 3];
-                                if (id == 0) continue;
-
-                                var attr = drive.SmartAttributes.GetAttribute(id);
-                                attr.Threshold = thresh;
-
-                                // Debug
-                                // Console.WriteLine("{0}\t {1}\t {2}\t {3}\t " + attr.Data + " " + ((attr.IsOK) ? "OK" : ""), attr.Name, attr.Current, attr.Worst, attr.Threshold);
-                            }
-                            catch (Exception ex)
-                            {
-                                // given key does not exist in attribute collection (attribute not in the dictionary of attributes)
-                                Debug.WriteLine(ex.Message);
-                            }
-                        }
-                    }
-
-                    #endregion
-
+                    AssignSmartValues(searcher, drive);
+                    AssignSmartThresholds(searcher, drive);
                     drives.Add(drive);
                 }
             }
@@ -98,6 +30,85 @@ namespace Simplified.IO
             }
 
             return drives;
+        }
+
+        private static ManagementObjectCollection GetDevices()
+        {
+            var deviceQuery = @"SELECT * FROM Win32_DiskDrive";
+            return new ManagementObjectSearcher(deviceQuery).Get();
+        }
+
+        private static void AssignSmartThresholds(ManagementObjectSearcher searcher, Drive drive)
+        {
+            var pnpDeviceID = drive.PnpDeviceID.Replace("\\", "\\\\");
+            var thresholdQuery = @"Select * from MSStorageDriver_FailurePredictThresholds Where InstanceName like ""%{0}%""";
+            searcher.Query = new ObjectQuery(string.Format(thresholdQuery, pnpDeviceID));
+
+            foreach (ManagementObject data in searcher.Get())
+            {
+                var bytes = (byte[])data.Properties["VendorSpecific"].Value;
+                for (var i = 0; i < 42; ++i)
+                {
+                    try
+                    {
+                        int id = bytes[i * 12 + 2];
+                        int thresh = bytes[i * 12 + 3];
+                        if (id == 0) continue;
+
+                        var attr = drive.SmartAttributes.GetAttribute(id);
+                        attr.Threshold = thresh;
+
+                        // Debug
+                        // Console.WriteLine("{0}\t {1}\t {2}\t {3}\t " + attr.Data + " " + ((attr.IsOK) ? "OK" : ""), attr.Name, attr.Current, attr.Worst, attr.Threshold);
+                    }
+                    catch (Exception ex)
+                    {
+                        // given key does not exist in attribute collection (attribute not in the dictionary of attributes)
+                        Debug.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+
+        private static void AssignSmartValues(ManagementObjectSearcher searcher, Drive drive)
+        {
+            var pnpDeviceID = drive.PnpDeviceID.Replace("\\", "\\\\");
+            var failureQuery = @"Select * from MSStorageDriver_FailurePredictData Where InstanceName like ""%{0}%""";
+            searcher.Query = new ObjectQuery(string.Format(failureQuery, pnpDeviceID));
+
+            foreach (ManagementObject smartValue in searcher.Get())
+            {
+                var bytes = (byte[])smartValue.Properties["VendorSpecific"].Value;
+                for (var i = 0; i < 42; ++i)
+                {
+                    try
+                    {
+                        int id = bytes[i * 12 + 2];
+
+                        int flags = bytes[
+                            i * 12 + 4]; // least significant status byte, +3 most significant byte, but not used so ignored.
+                        //bool advisory = (flags & 0x1) == 0x0;
+                        var failureImminent = (flags & 0x1) == 0x1;
+                        //bool onlineDataCollection = (flags & 0x2) == 0x2;
+
+                        int value = bytes[i * 12 + 5];
+                        int worst = bytes[i * 12 + 6];
+                        var vendordata = BitConverter.ToInt32(bytes, i * 12 + 7);
+                        if (id == 0) continue;
+
+                        var attr = drive.SmartAttributes.GetAttribute(id);
+                        attr.Current = value;
+                        attr.Worst = worst;
+                        attr.Data = vendordata;
+                        attr.IsOK = failureImminent == false;
+                    }
+                    catch (Exception ex)
+                    {
+                        // given key does not exist in attribute collection (attribute not in the dictionary of attributes)
+                        Debug.WriteLine(ex.Message);
+                    }
+                }
+            }
         }
 
         private static bool GetOverallStatus(ManagementObjectSearcher searcher, Drive drive)
